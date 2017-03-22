d84b8d98a6917f05b9ffbb590427ed5a7d97f1b8
id093108 <id093108@BGC-BE-6BA1FD44.BGC.NET>
2017-03-22 19:53:40 +0100

    open ssh sampler test classes added

---
+++ jmeter-ssh-sampler/src/test/java/org/apache/jmeter/protocol/ssh/test/TestOpenSSHSessionSampler.java
@@ -0,0 +1,123 @@
+package org.apache.jmeter.protocol.ssh.test;
+ 
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.jmeter.protocol.ssh.sampler.SSHCommandSamplerExtra;
+import org.apache.jmeter.protocol.ssh.sampler.SSHOpenPersistentShellSampler;
+import org.apache.jmeter.protocol.ssh.sampler.SSHPersistentShellSendCommandSampler;
+import org.apache.jmeter.protocol.ssh.sampler.SshSession;
+import org.apache.jmeter.protocol.ssh.sampler.GlobalDataSsh;
+import org.apache.jmeter.protocol.ssh.sampler.OpenSSHSessionSampler;
+import org.apache.jmeter.samplers.SampleResult;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.jcraft.jsch.Session;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsEqual;
+import jline.internal.Log;
+import org.junit.Assert;
+
+
+public class TestOpenSSHSessionSampler {
+	private final static Logger LOG  =Logger.getLogger(TestOpenSSHSessionSampler.class.getName());
+	private OpenSSHSessionSampler instance = null;
+	static private Thread sshts=null;
+
+	public TestOpenSSHSessionSampler() {
+		//super(name);
+	}
+	//Sometimes several tests need to share computationally expensive setup (like logging into a database).	
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		sshts = new Thread(new SSHTestServer());
+		sshts.start();
+		Thread.sleep(1000);
+		Log.info("ssh test server thread started");
+	}
+	
+	//If you allocate expensive external resources in a
+	//BeforeClass method you need to release them after all the tests in the class have run.
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    	sshts.interrupt();
+    	
+    }
+    
+    //When writing tests, it is common to find that several tests need similar objects created before they can run
+    @Before
+	public void setUp() throws Exception {
+		//super.setUp();
+		 
+			this.instance = new OpenSSHSessionSampler();  
+			//LOG.log(Level.INFO,"created new instance of SSHCommandSamplerExtra");
+		 
+	}
+    //If you allocate external resources in a Before method you need to release them after the test runs.
+    @After
+	public void tearDown() throws Exception {
+		//super.tearDown();
+		this.instance = null;//  close connection and cleanup of globaldata in test itself
+	}
+    //The Test annotation tells JUnit that the public void method to which it is attached can be run as a test case.
+	@Test 
+	public void testOpenSSHSessionSampler() {
+		 this.instance=new OpenSSHSessionSampler();           
+		 this.instance.setConnectionName("CONN1");
+		 this.instance.setConnectionTimeout(3200);
+		 this.instance.setHostname("127.0.0.1");
+		 this.instance.setPassword("azerty!");
+		 this.instance.setPort(5222);
+		 this.instance.setUsername("johan"); 
+		 SampleResult sr= this.instance.sample(null) ;
+		 Integer errorCount= sr.getErrorCount();
+		 LOG.log(Level.INFO, "errorcount:"+ Integer.toString(errorCount));
+		 assertTrue("ErrorCount is 0",errorCount==0);
+		 LOG.log(Level.INFO, "content type:"+sr.getContentType());
+		 String responseCode=sr.getResponseCode();
+		 LOG.log(Level.INFO, "response code:"+responseCode);
+		 assertTrue(responseCode.equals("0"));
+		 String responseMessage=sr.getResponseMessage();
+		 LOG.log(Level.INFO, "response message:"+responseMessage);
+		 assertTrue("response message is Connected",
+				 responseMessage.equals("Connected") );		
+		 String responseData=sr.getResponseDataAsString();
+		 LOG.log(Level.INFO, "response data as string:"+responseData);
+		 assertTrue("response data is Connected",
+				 responseData.equals("Connected") );
+		 String responseLabel= sr.getSampleLabel();
+		 LOG.log(Level.INFO, "response label as string:"+responseLabel);
+		 assertTrue(" responseLabel is OpenSSHSessionSampler Connect (johan@127.0.0.1)",
+				 responseLabel.equals("OpenSSHSessionSampler Connect (johan@127.0.0.1)") );
+		 String responseSamplerData = sr.getSamplerData();
+		 LOG.log(Level.INFO, "response sampler data as string:"+responseSamplerData);
+		 assertTrue("responseSamplerData is Open SSH connection (johan@127.0.0.1) with name: CONN1",
+				 responseSamplerData.equals("Open SSH connection (johan@127.0.0.1) with name: CONN1") );
+		 
+		 SshSession sess=GlobalDataSsh.GetSessionByName("CONN1");
+		 assertTrue("session is not null", sess!=null);
+		 
+		 // clean up before assert
+		 if (sess !=null)
+		 {
+			 try {
+				 sess.disconnect(); // closes all shells
+			 }
+			 catch(Exception e) {
+				 LOG.log(Level.INFO, "disconnect failed" );
+			 }
+			 LOG.log(Level.INFO, "removing ssh session GlobalDataSsh from CONN1" );
+
+			 GlobalDataSsh.removeSession("CONN1");
+		 }
+		 
+	}
+	
+}
